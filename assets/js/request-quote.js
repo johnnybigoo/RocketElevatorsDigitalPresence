36f2407769ae03964f1ec04eb358469fd4ef4288
johnnybigoo <jotalico@icloud.com>
2021-10-01 15:09:52 -0400

    Genesis-Day#10-1

--- assets/js/request-quote.js
+++ assets/js/request-quote.js
@@ -1,3 +1,17 @@
+const userAction = async () => {
+    const response = await fetch('http://localhost:8081', {
+        method: 'POST',
+        body: myBody, // string or object
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    const myJson = await response.json(); //extract JSON from the http response
+    // this is where you can assign the returned value to the right html element
+}
+
+
+
 // Variables
 var numApartments = 0;
 var numFloorsRes = 0;
